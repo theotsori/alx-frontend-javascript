2c70d7bf376b3c7795efcb5f21714dd8472af9d0
theotsori <theodoretsori@gmail.com>
2023-06-27 14:55:00 +0300

    No need to create empty objects

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,10 +1,9 @@
 export default function getBudgetObject(income, gdp, capita) {
-  // eslint-disable-next-line object-shorthand
   const budget = {
-    income: income,
-    gdp: gdp,
-    capita: capita,
+    income,
+    gdp,
+    capita,
   };
-  
+
   return budget;
 }
