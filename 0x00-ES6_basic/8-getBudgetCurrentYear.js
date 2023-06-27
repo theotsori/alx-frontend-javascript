9c518295ef25846c1d5f909202e35102e962228f
theotsori <theodoretsori@gmail.com>
2023-06-27 15:55:09 +0300

    Update empty objects

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -7,8 +7,8 @@ export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
-    [`capita-${getCurrentYear()}`]: capita,
-  }
+    [`capita-${getCurrentYear()}`]: capita
+  };
 
   return budget;
 }
