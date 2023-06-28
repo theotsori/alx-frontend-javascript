1f6853bd38f207327fdf978fca91b075e958180a
theotsori <theodoretsori@gmail.com>
2023-06-28 11:01:37 +0300

    keep every promise you make and only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+export default function getResponseFromAPI () {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const response = { message: 'API response' };
+      resolve(response);
+    }, 1000);
+  });
+}
