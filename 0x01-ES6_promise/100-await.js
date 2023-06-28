36465ae429e68c79a51ffeba1758adaec644cd8b
theotsori <theodoretsori@gmail.com>
2023-06-28 16:36:19 +0300

    Await/Async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,18 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const photoResponse = await uploadPhoto('filename.jpg');
+    const userResponse = await createUser('Guillaume', 'Salva');
+
+    return {
+      photo: photoResponse,
+      user: userResponse
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null
+    };
+  }
+}
