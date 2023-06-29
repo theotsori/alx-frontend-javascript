25484942e851cd074fedb0842246cba8c817e818
theotsori <theodoretsori@gmail.com>
2023-06-29 22:46:18 +0300

    ES6 inheritance

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -15,7 +15,7 @@ class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
 
