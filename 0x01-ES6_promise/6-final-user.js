bbeca7f52a08f5c89ff03bb7589b7ec91d8a912b
theotsori <theodoretsori@gmail.com>
2023-06-30 16:22:17 +0300

    final-user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,22 +1,22 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([userPromise, photoPromise])
-    .then((results) =>
-      results.map((result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
-      }))
-    )
-    .then((data) => {
-      console.log(data);
-      return data;
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  try {
+    const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
+
+    const userStatus = userResult.status === 'fulfilled' ? 200 : 500;
+    const photoStatus = photoResult.status === 'fulfilled' ? 200 : 500;
+
+    return [
+      { status: userStatus, value: userResult.status === 'fulfilled' ? userResult.value : new Error() },
+      { status: photoStatus, value: photoResult.status === 'fulfilled' ? photoResult.value : new Error() },
+    ];
+  } catch {
+    console.log('Signup system offline');
+    return [];
+  }
 }
