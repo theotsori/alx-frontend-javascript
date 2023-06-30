2738a9c4a915b22a75d479decc8748e6407ef9f3
theotsori <theodoretsori@gmail.com>
2023-06-30 16:33:30 +0300

    final-user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,17 +1,11 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-
-  return Promise.all(promises).then((results) => {
-    const array = results.map((result) => {
-      return {
-        status: result.status,
-        value: result.value,
-      };
-    });
-
-    return array;
-  });
+export default function handleProfileSignup(firstname, lastname, filename) {
+  return Promise.all([
+    signUpUser(firstname, lastname),
+    uploadPhoto(filename),
+  ])
+  .then(values => values)
+  .catch(err => err);
 }
