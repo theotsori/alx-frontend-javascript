cb7d77e0fb43c328d1254be9d3f5c68bb480ab3c
theotsori <theodoretsori@gmail.com>
2023-07-03 16:38:53 +0300

    Weak link data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [name, quantity] of map.entries()) {
+    if (quantity === 1) {
+      map.set(name, 100);
+    }
+  }
+}
