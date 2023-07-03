061221804e96c4b8cd288e5cfdac8f26074cfa8a
theotsori <theodoretsori@gmail.com>
2023-07-03 13:43:57 +0300

    reduce

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,17 +1,17 @@
 export default function getListStudents() {
   return [
     {
-      id: '1',
+      id: 1,
       firstName: 'Guillaume',
       location: 'San Francisco',
     },
     {
-      id: '2',
+      id: 2,
       firstName: 'James',
       location: 'Columbia',
     },
     {
-      id: '5',
+      id: 5,
       firstName: 'Serena',
       location: 'San Francisco',
     },
